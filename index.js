35bc49e905e5d350795cb5dbbe47e0c09870c0ce
Tesfaye Lemi <tesfu9503@gmail.com>
2022-09-11 10:54:29 -0700

    make it better

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-import express from 'express'
-import mongoose  from 'mongoose'
-import dotenv  from 'dotenv'
-import cors from 'cors'
-
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from 'body-parser'
+// routes
+import auth from './routes/auth.js'
+import owner from './routes/owner.js'
 import property from './routes/property.js'
 import agent from './routes/agent.js'
-import owner from './routes/owner.js'
-import auth from './routes/auth.js'
-dotenv.config()
-const app=express()
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+  
 mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log('db connecdtionsss successful'))
+.then(()=>console.log('db connection successful'))
 .catch((e)=>{console.log(e)})
 app.get('/',(req,res)=>{
     res.send("successful")
 })
 
-
-app.use(express.json({ limit: '30mb', extended: true }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
-
-// app.use('/api/owner',owner)
+app.use('/api/auth',auth)
+app.use('/api/owner',owner)
 app.use('/api/property',property)
 app.use('/api/agent',agent)
+
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{console.log("server is running")})
-
