59d5aeafdd0ffecedb5238eef8bba722339629b8
Tesfaye Lemi <tesfu9503@gmail.com>
2022-09-11 10:42:35 -0700

    make it better

--- routes/owner.js
+++ routes/owner.js
@@ -4,12 +4,11 @@ const router = express.Router();
 
 import {
   createOwner,getAllOwners,getSingleCompany,deleteSingleOwner,updateOwner
-} from "../Controllers/owner.js";
+} from "../Controllers/Owner.js";
 
 router.post("/create", createOwner);
 router.get("/", getAllOwners);
 router.get("/find/:id", getSingleCompany);
 router.delete("/find/:id", deleteSingleOwner);
 router.put("/find/:id", updateOwner);
-
 export default router;
