755541a4e2d1e62af752fd777d39644ad59da57a
Tesfaye Lemi <tesfu9503@gmail.com>
2022-10-27 05:13:33 -0700

    banner selected prop send to the application

--- Models/AdBanner.js
+++ Models/AdBanner.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 const bannerAdSchema=new mongoose.Schema({
-    user:{type:mongoose.SchemaTypes.ObjectId,ref:'User'},
-    owner:{type:mongoose.SchemaTypes.ObjectId,ref:'Owner'},
-    AgentCompany:{type:String},
+    user:{type:mongoose.SchemaTypes.ObjectId,ref:'User',default:null},
+    owner:{type:mongoose.SchemaTypes.ObjectId,ref:'Owner',default:null},
+    AgentCompany:{type:String,default:null},
     image:{type:String,required:true},
-    name:{type:String},
+    name:{type:String,default:null},
     isAccepted:{type:Boolean,default:false},
     isRejected:{type:Boolean,default:false},
 },{timestamps:true})
