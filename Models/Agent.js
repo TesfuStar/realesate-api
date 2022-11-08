aa5eef1fc05b902ab9c7fbf46d1ccc8cd3552898
Tesfaye Lemi <tesfu9503@gmail.com>
2022-11-08 05:33:25 -0800

    final commit for argent case

--- Models/Agent.js
+++ Models/Agent.js
@@ -4,7 +4,7 @@ const AgentSchema = new mongoose.Schema(
     companyId: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    profile: {type: String,},
+    profile: {type: String,default:null},
     phone: { type: [String], required: true,unique:true },
     email: { type: String,default:null },
     hasCompany: { type: Boolean, default: false },
